6921377190e74f0a7a8c8648969e4b9a1276034f
ob0117 <63119382+ob0117@users.noreply.github.com>
2020-05-28 18:08:17 -0400

    primitive looping button
    - if you press loop instead of record again, you can make the recording loop
    - you can record over the loop and add new stuff, but this version has audio files and sometimes they don't play smoothly at same time
    - still sounds sketchy cuz it doesn't fit within certain bars, it just starts looping when you click

--- Ilab Final Project/Assets/Scripts/RecordButton.cs
+++ Ilab Final Project/Assets/Scripts/RecordButton.cs
@@ -7,26 +7,42 @@ using UnityEngine.UI;
 public class RecordButton : MonoBehaviour
 {
 
+    // booleans that keep track of whether or not functions can be called
     public bool recording;
+    bool canPress;
+    bool canInvoke;
 
+    // these arrays store buttons and times they were clicked during recording
     List<float> recTimes;
     List<Button> buttons;
+
+    // this adds some lag time before loops but the value's kinda random
+    // we'll need to let them select a tempo and have the loops fit in set bars
+    public float endDelay = 0.5f;
 
     void Start()
     {
         recording = false;
+        canPress = true;
+
         recTimes = new List<float>();
         buttons = new List<Button>();
+
+        canInvoke = true;
     }
 
     public void Record()
     {
-        recording = !recording;
-
-        if(recording == false)
+        // canPress prevents you from recording while it's playing
+        if(canPress)
         {
-            // call the playback function
-            StartCoroutine(PlayBack());
+            recording = !recording;
+
+            if (recording == false && recTimes.Count > 0)
+            {
+                // call the playback function
+                StartCoroutine(PlayBack());
+            }
         }
 
     }
@@ -35,6 +51,7 @@ public class RecordButton : MonoBehaviour
     // it'll use the arrays to play back the notes with the right time intervals
     private IEnumerator PlayBack()
     {
+        canPress = false;
 
         buttons[0].onClick.Invoke();
         
@@ -44,21 +61,72 @@ public class RecordButton : MonoBehaviour
             buttons[i].onClick.Invoke();
         }
 
-        // at the end of playback, reset recording arrays
+        // at the end of playback, reset recording arrays and allow recording again
         recTimes = new List<float>();
         buttons = new List<Button>();
 
+        canPress = true;
+
     }
 
-    // this is added to notes 
+    // this is attached to the note and chord buttons to track when they're pressed while recording
     public void NoteClick(string name)
     {
-        if (recording)
+        if (recording && canInvoke)
         {
             // log time of note click and audio clip
             recTimes.Add(Time.time);
             buttons.Add(GameObject.Find(name).GetComponent<Button>());
         }
     }
+
+    public void AddLoop()
+    {
+        recording = false;
+
+        if(recTimes.Count > 0)
+        {
+            // making temporary copies of the arrays to call the loop
+
+            List<float> recTimesTemp = new List<float>();
+            List<Button> buttonsTemp = new List<Button>();
+
+            for(int i = 0; i < recTimes.Count; i++)
+            {
+                recTimesTemp.Add(recTimes[i]);
+                buttonsTemp.Add(buttons[i]);
+            }
+
+            // loops current recording
+            StartCoroutine(Loop(recTimesTemp, buttonsTemp));
+
+            recTimes = new List<float>();
+            buttons = new List<Button>();
+        }
+
+    }
+
+
+    private IEnumerator Loop(List<float> t, List<Button> b)
+    {
+        while(true)
+        {
+            // call the button sounds, but don't let the sounds that are being looped get recorded
+            canInvoke = false;
+            b[0].onClick.Invoke();
+            canInvoke = true;
+
+            for (int i = 1; i < t.Count; i++)
+            {
+                yield return new WaitForSeconds(t[i] - t[i - 1]);
+                canInvoke = false;
+                b[i].onClick.Invoke();
+                canInvoke = true;
+            }
+
+            yield return new WaitForSeconds(endDelay);
+        }
+    }
+
 
 }
