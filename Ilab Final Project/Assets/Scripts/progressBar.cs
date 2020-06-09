05e8733c336bd19d4f883be42cf77f6b553f5ce4
Biosaur <biosaur99@gmail.com>
2020-06-09 18:12:25 -0400

    Progress Bar Skeleton

--- Ilab Final Project/Assets/Scripts/progressBar.cs
+++ Ilab Final Project/Assets/Scripts/progressBar.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlaybackButton : MonoBehaviour
+public class progressBar : MonoBehaviour
 {
+    RectTransform progress;
+    int x;
     // Start is called before the first frame update
     void Start()
     {
-        
+        progress = gameObject.GetComponent<RectTransform>();
+        x = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        x++;
+        progress.localPosition = new Vector3(x,0,0);
     }
 }
